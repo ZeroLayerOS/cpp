019be7373ac79d90b1f4f855460ac1bfc8c60b74
Ziad Ali <zalshemy9@gmail.com>
2026-08-24 16:08:55 +0300

    solve basic data type problem

---
+++ basic Data Types.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define LL long long
+
+using namespace std;
+
+void fast_io()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+int main (void)
+{
+    fast_io;
+
+    int number;
+    LL big_number;
+    char ch;
+    float fl;
+    double dou;
+    
+    cin >> number >> big_number >> ch >> fl >> dou;
+
+    cout << number << "\n" << big_number << "\n" << ch << "\n" << fl << "\n" << dou ;
+
+    
+}
