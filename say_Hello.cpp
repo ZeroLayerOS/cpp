d2504e17866a9574b0535031371e65981676a30e
Ziad Ali <zalshemy9@gmail.com>
2026-08-24 15:39:49 +0300

    solve 2 problems

---
+++ say_Hello.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void fast_io()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+int main(void)
+{
+    fast_io();
+    string s;
+    cin >> s;
+    cout << "Hello," << " "<< s << "\n";
+
+}
