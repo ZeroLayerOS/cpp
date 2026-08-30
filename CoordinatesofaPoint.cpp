0412152b9f46f528def64cab12f62a73a188e097
Ziad Ali <zalshemy9@gmail.com>
2026-08-30 12:13:32 +0300

    solve  Coordinates of a Point problem

---
+++ CoordinatesofaPoint.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main (void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    float X, Y;
+    cin >> X >> Y;
+    
+    if (X == 0 && Y == 0)
+    {
+        cout << "Origem" << "\n";
+    }
+    else if (X > 0 && Y > 0)
+    {
+        cout << "Q1" << "\n";
+    }
+    else if (X < 0 &&  Y > 0)
+    {
+       cout << "Q2" << "\n";
+    }
+    else if ( X < 0 && Y < 0)
+    {
+         cout << "Q3" << "\n";
+    }
+    else if (X > 0 && Y < 0)
+    {
+         cout << "Q4" << "\n";
+    }
+    else if (X == 0 && Y != 0)
+    {
+      cout << "Eixo Y";
+    }
+    else if (Y == 0 && X != 0)
+    {
+         cout << "Eixo X";
+    }
+}
