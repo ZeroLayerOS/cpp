d3340e9f697e1ae627917dbff073f24b4d38082c
Ziad Ali <zalshemy9@gmail.com>
2026-09-06 00:19:43 +0300

    solve comparison problem

---
+++ Comparison.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int a, b;
+    char s;
+
+
+    cin >> a >> s >> b;
+
+    if (s == '>')
+    {
+        if (a > b) cout << "Right\n";
+        else cout << "Wrong\n";
+    }
+    else if (s == '<')
+    {
+        if (a < b) cout << "Right\n";
+        else cout << "Wrong\n";
+    }
+    else if (s == '=')
+    {
+        if (a == b) cout << "Right\n";
+        else cout << "Wrong\n";
+    }
+
+    return 0;
+}
